4b52eb9f8534da8b8255da32c1e13c6a105a7c84
Jackson Gray <jacksongray36@gmail.com>
2019-05-28 15:26:01 +1000

    Mistakes have been made

---
+++ Cancer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mistake
+{
+    class Cancer
+    {
+        public List<List<List<List<List<int>>>>> Mistakes = new List<List<List<List<List<int>>>>>();
+        private int level2list = 0;
+        private int level3list = 0;
+        private int level4list = 0;
+        private int level5list = 0;
+        private int fullLists = 0;
+        public Cancer()
+        {
+            Mistakes.Add(new List<List<List<List<int>>>>());
+            Mistakes[0].Add(new List<List<List<int>>>());
+            Mistakes[0][0].Add(new List<List<int>>());
+            Mistakes[0][0][0].Add(new List<int>());
+        }
+        public void FuckComputer(int current)
+        {
+            for (int i = 0; i < current; i++)
+            {
+                
+                if (current != 1)
+                {
+                    FuckComputer(current - 1);
+                }
+                else
+                {
+                    try
+                    {
+                        Mistakes[level5list][level4list][level3list][level2list].Add(1);
+                    }
+                    catch(OutOfMemoryException)
+                    {
+                        try
+                        {
+                            Mistakes[level5list][level4list][level3list].Add(new List<int>());
+                            fullLists++;
+                            Console.WriteLine("Running total: {0}\nFilled lists: {1}", Mistakes[level5list][level4list][level3list][level2list].Count * fullLists, fullLists);
+                            level2list++;
+                            Mistakes[level5list][level4list][level3list][level2list].Add(1);
+                        }
+                        catch (OutOfMemoryException)
+                        {
+                            try
+                            {
+                                Mistakes[level5list][level4list].Add(new List<List<int>>());
+                                level3list++;
+                                level2list = 0;
+                                Mistakes[level5list][level4list][level3list].Add(new List<int>());
+                                Mistakes[level5list][level4list][level3list][level2list].Add(1);
+                            }
+                            catch (OutOfMemoryException)
+                            {
+                                try
+                                {
+                                    Mistakes[level5list].Add(new List<List<List<int>>>());
+                                    level4list++;
+                                    level3list = 0;
+                                    level2list = 0;
+                                    Mistakes[level5list][level4list].Add(new List<List<int>>());
+                                    Mistakes[level5list][level4list][level3list].Add(new List<int>());
+                                    Mistakes[level5list][level4list][level3list][level2list].Add(1);
+                                }
+                                catch (OutOfMemoryException)
+                                {
+                                    level5list++;
+                                    level4list = 0;
+                                    level3list = 0;
+                                    level2list = 0;
+                                    Mistakes.Add(new List<List<List<List<int>>>>());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Mistake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number factorial do you want to know?");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Cancer mistake = new Cancer();
+            mistake.FuckComputer(length);
+            BigInteger total = new BigInteger(0);
+            foreach (List<List<List<List<int>>>> i in mistake.Mistakes)
+            {
+                foreach (List<List<List<int>>> k in i)
+                {
+                    foreach (List<List<int>> j in k)
+                    {
+                        foreach (List<int> l in j)
+                        {
+                            foreach (int m in l) { total += m; }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Total of {0}!: {1}", length, total);
+            Console.ReadLine();
+        }
+    }
+}
